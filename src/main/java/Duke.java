15040670fd7e1c9ccf83815b02af51b19e99cd3f
DESKTOP-BUIJDKP\amand <amandachuasineng@gmail.com>
2020-08-23 12:24:32 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,22 +6,30 @@ public class Duke {
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
         line = in.nextLine();
-
-        while (!line.equals("bye")) {
+        while(!line.equals("bye")) {
             if (line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for(int i=0; i<tasks.size(); i++){
                     System.out.println((i+1) + "." + tasks.get(i));
                 }
             }
+            else if(line.startsWith("done")){
+                int taskNo = Integer.parseInt(line.substring(5));
+                Task chosenTask = tasks.get(taskNo-1);
+                chosenTask.markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" " + chosenTask);
+            }
             else {
+                Task newTask = new Task(line);
                 System.out.println("added: " + line);
-                tasks.add(line);
+                tasks.add(newTask);
 
             }
             line = in.nextLine();
