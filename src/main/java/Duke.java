94751c939d7df40462e99f6d3992a961ba790de1
DESKTOP-BUIJDKP\amand <amandachuasineng@gmail.com>
2020-09-04 15:21:00 +0800

    Level-5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,16 @@ import java.util.ArrayList;
 
 public class Duke {
 
+    public static void printTask(Task taskType,  ArrayList<Task> tasks){
+        System.out.println("Got it. I've added this task:");
+        System.out.println("   " + taskType);
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.\n");
+    }
+
     public static void main(String[] args) {
-        String line;
         Scanner in = new Scanner(System.in);
-        ArrayList<Task> tasks = new ArrayList<Task>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         String logoStart = "       _____________       \n"
                 + "      /             \\     \n"
@@ -26,8 +32,8 @@ public class Duke {
         System.out.println(logoStart);
         System.out.println("What can I do for you?");
 
-        line = in.nextLine();
-        while(!line.equals("bye")) {
+        while(!in.hasNext("bye")) {
+            String line = in.nextLine();
             if(line.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for(int i=0; i<tasks.size(); i++) {
@@ -42,39 +48,57 @@ public class Duke {
                 System.out.println(" " + chosenTask);
             }
             else if(line.startsWith("todo")){
-                Task toDo = new ToDo(line.replace("todo ", ""));
-                tasks.add(toDo);
-                printTask(toDo, tasks);
+                int noOfWords = 0;
+                for (String description : line.split(" ")) {
+                    noOfWords++;
+                }
+                if (noOfWords > 1) {
+                    Task toDo = new ToDo(line.replace("todo ", ""));
+                    tasks.add(toDo);
+                    printTask(toDo, tasks);
+                }
+                else{
+                    System.out.println("-------------------------------------------------------------------\n" +
+                            "☹ OOPS!!! The description of a todo cannot be empty.\n" +
+                            "-------------------------------------------------------------------\n");
+                }
             }
             else if(line.startsWith("deadline")){
-                String date = line.split("/")[1];
-                String description = line.split("/")[0].replace("deadline ", "");
-                Task taskDeadline = new Deadline(description, date);
-                tasks.add(taskDeadline);
-                printTask(taskDeadline, tasks);
+                try {
+                    String date = line.split("/")[1];
+                    String description = line.split("/")[0].replace("deadline ", "");
+                    Task taskDeadline = new Deadline(description, date);
+                    tasks.add(taskDeadline);
+                    printTask(taskDeadline, tasks);
+                }
+                catch (ArrayIndexOutOfBoundsException ex){
+                    System.out.println("-------------------------------------------------------------------\n" +
+                            "☹ OOPS!!! The deadline input cannot be empty.\n" +
+                            "-------------------------------------------------------------------\n");
+                }
             }
             else if(line.startsWith("event")){
-                String time = line.split("/")[1];
-                String description = line.split("/")[0].replace("deadline ", "");
-                Task taskEvent = new Event(description, time);
-                tasks.add(taskEvent);
-                printTask(taskEvent, tasks);
+                try {
+                    String time = line.split("/")[1];
+                    String description = line.split("/")[0].replace("event ", "");
+                    Task taskEvent = new Event(description, time);
+                    tasks.add(taskEvent);
+                    printTask(taskEvent, tasks);
+                }
+                catch (ArrayIndexOutOfBoundsException ex){
+                    System.out.println("-------------------------------------------------------------------\n" +
+                            "☹ OOPS!!! The event input cannot be empty.\n" +
+                            "-------------------------------------------------------------------\n");
+                }
             }
             else {
-                Task newTask = new Task(line);
-                System.out.println("added: " + line);
-                tasks.add(newTask);
+                System.out.println("-------------------------------------------------------------------\n" +
+                        "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n" +
+                        "-------------------------------------------------------------------\n");
             }
-            line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(logoEnd);
     }
 
-    public static void printTask(Task taskType,  ArrayList<Task> tasks){
-        System.out.println("Got it. I've added this task:");
-        System.out.println("   " + taskType);
-        System.out.println("---------------------------------------------------------------------------");
-        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-    }
 }
